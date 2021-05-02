bfa509b8607792c058d1b32cb43e1bdef86a7a40
zhongqi <imzhongqi@gmail.com>
2021-05-02 21:26:57 +0800

    remove code

--- reporter.go
+++ reporter.go
@@ -2,8 +2,6 @@ package reporter
 
 import (
 	"context"
-	"math/rand"
-	"time"
 )
 
 var DefaultReporter Reporter = &nopReporter{}
@@ -23,27 +21,4 @@ func (n nopReporter) Report(ctx context.Context, message Message) error { return
 
 func Report(ctx context.Context, message Message) error {
 	return DefaultReporter.Report(ctx, message)
-}
-
-type reporterPool struct {
-	rand.Source
-	reporterList []Reporter
-}
-
-func NewReporterPool(reporterList ...Reporter) Reporter {
-	return &reporterPool{
-		Source:       rand.NewSource(time.Now().UnixNano()),
-		reporterList: reporterList,
-	}
-}
-
-func (r *reporterPool) Report(ctx context.Context, message Message) error {
-	reporterNum := len(r.reporterList)
-	if reporterNum == 0 {
-		return nil
-	}
-	if reporter := r.reporterList[int(r.Int63())%reporterNum]; reporter != nil {
-		return reporter.Report(ctx, message)
-	}
-	return nil
 }
